1c15af85816973e8be5c451369b80aa0fca83c4f
lucas <mcbride.lucas9@gmail.com>
2016-11-15 09:56:01 -0500

    adjust columns

--- usc_app/table.py
+++ usc_app/table.py
@@ -4,7 +4,6 @@ from django.core.urlresolvers import reverse
 from django_tables2.utils import A
 
 class RosterTable(tables.Table):
-    id = tables.Column(orderable=False)
     team = tables.LinkColumn('team',args=[A('team')], verbose_name="Team",)
     rank = tables.Column(verbose_name="Rank")
     captain = tables.Column(orderable=False, verbose_name="Captain")
@@ -43,11 +42,10 @@ class RosterTable(tables.Table):
         verbose_name="Member",
         # default="",
     )
-    active = tables.Column(orderable=False, verbose_name="Active")
     server = tables.Column(verbose_name="Server")
     class Meta:
-        model = Roster
-        exclude = ('id', )
+        model = TeamRoster
+        exclude = ('id','firstActive','daysActive','logo')
         attrs = {'class': 'table'}
         row_attrs = {
             'id': lambda record: 'R' + str(record.rank)
