ee477dc1eb7e97dba4773f695a50ca39e3633947
lucas <mcbride.lucas9@gmail.com>
2016-11-25 13:37:39 -0500

    change to roster and stats

--- usc_app/models.py
+++ usc_app/models.py
@@ -1,17 +1,77 @@
 from django.db import models
 
 
-class TeamRoster(models.Model):
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    minutes = models.PositiveIntegerField(blank=True,null=True)
+    tags = models.PositiveIntegerField(blank=True,null=True)
+    captures = models.PositiveIntegerField(blank=True,null=True)
+    hold = models.CharField(max_length=10,blank=True,null=True)
+    returns = models.PositiveIntegerField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Roster(models.Model):
     team = models.CharField(max_length=50)
     rank = models.PositiveIntegerField()
-    captain = models.CharField(max_length=50)
-    co_captain = models.CharField(max_length=50)
-    member1 = models.CharField(max_length=50)
-    member2 = models.CharField(max_length=50)
-    member3 = models.CharField(max_length=50,blank=True)
-    member4 = models.CharField(max_length=50,blank=True)
-    member5 = models.CharField(max_length=50,blank=True)
-    member6 = models.CharField(max_length=50,blank=True)
+    captain = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='captain',
+        verbose_name='Captain',
+    )
+    co_captain = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='co_captain',
+        verbose_name='Co-Captain',
+    )
+    member1 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member1',
+        verbose_name='Member1',
+    )
+    member2 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member2',
+        verbose_name='Member2',
+    )
+    member3 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member3',
+        verbose_name='Member3',
+        blank=True,
+        null=True
+    )
+    member4 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member4',
+        verbose_name='Member4',
+        blank=True,
+        null=True
+    )
+    member5 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member5',
+        verbose_name='Member5',
+        blank=True,
+        null=True
+    )
+    member6 = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='member6',
+        verbose_name='Member6',
+        blank=True,
+        null=True
+    )
     firstActive = models.CharField(max_length=50)
     daysActive = models.PositiveIntegerField()
     server = models.CharField(max_length=50)
@@ -24,7 +84,7 @@ class TeamRoster(models.Model):
     class Meta:
         db_table = 'roster'
 
-class TeamStats(models.Model):
+class Stats(models.Model):
      change = models.IntegerField(null=True,blank=True)
      team = models.CharField(max_length=50)
      streak = models.IntegerField(null=True,blank=True)
--- usc_app/views.py
+++ usc_app/views.py
@@ -10,27 +10,29 @@ def index(request):
     return render(request, 'index.html')
 
 def roster_table(request):
-    queryset = TeamRoster.objects.all()
+    queryset = Roster.objects.all()
     table = RosterTable(queryset)
+    players = Player.objects.all()
     table.order_by = 'rank'
     RequestConfig(request, paginate={
         'per_page': 38
     }).configure(table)
-    return render(request, 'roster_table.html', {'table': table,})
+    return render(request, 'roster_table.html', {
+        'table': table,
+        'players':players,
+    })
 def team_page(request,team_name):
-    rosters = TeamRoster.objects.all().order_by('rank')
-    # team = get_object_or_404(TeamRoster,team=team_name)
-    # stats = get_object_or_404(TeamStats,team=team_name)
-    team = TeamRoster.objects.filter(team=team_name).first()
-    stats = TeamStats.objects.filter(team=team_name).first()
-    queryset = TeamStats.objects.filter(team=team.team)
+    rosters = Roster.objects.all().order_by('rank')
+    team = Roster.objects.filter(team=team_name).first()
+    stats = Stats.objects.filter(team=team_name).first()
+    queryset = Stats.objects.filter(team=team.team)
     table = StatsTable(queryset)
     challengers = {}
     rank = int(team.rank)
     for roster in rosters:
         # Create list of challengers (teams 4 spots above current team)
         if rank - roster.rank <= 4 and rank - roster.rank > 0:
-            challengers[roster.rank]= get_object_or_404(TeamStats, team=roster.team)
+            challengers[roster.rank]= get_object_or_404(Stats, team=roster.team)
     sorted_challengers = sorted(challengers.items(), key=operator.itemgetter(0)) #Sort challengers list by key(rank)
     return render(request,'team_page.html',{
         'team':team, # for team page
@@ -41,7 +43,9 @@ def team_page(request,team_name):
         'rank':rank # for page header
     });
 def player_page(request,player_name):
-    rosters = TeamRoster.objects.all().order_by('rank')
+    rosters = Roster.objects.all().order_by('rank')
+    player = Player.objects.filter(name=player_name).first()
     return render(request,'player_page.html',{
         'rosters':rosters,
+        'player':player,
     })
